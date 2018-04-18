4b5df153e488f4e22771a455305b53c0bb7e7196
Ganesh Arvinth <ganesharvinth0@gmail.com>
2018-04-18 10:40:00 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,17 @@
+def display_board (elements)
+  puts (" #{elements[0]} | #{elements[1]} | #{elements[2]} ")
+  puts ("-----------")
+  puts (" #{elements[3]} | #{elements[4]} | #{elements[5]} ")
+  puts ("-----------")
+  puts (" #{elements[6]} | #{elements[7]} | #{elements[8]} ")
+end
+
+def turn (board)
+  userInput = gets.strip
+  index = userInput.to_i
+
+  if ( board[index] == "X" || board[index] == "O")
+    turn (board)
+  else
+    board [index] = "X"
+  end
